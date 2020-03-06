895e3f1c909ca8c89066d10b882d658593c30f62
al Mohamady <al.Mohamady47@gmail.com>
2020-03-06 15:53:52 +0200

    built the chat room

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,17 +1,108 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 
 class Chat extends StatefulWidget {
 
   static const String id = "CHAT";
+  final FirebaseUser user;
+
+  const Chat( {Key key, this.user}) : super(key: key);
 
   @override
   _ChatState createState() => _ChatState();
 }
 
 class _ChatState extends State<Chat> {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final Firestore _firestore = Firestore.instance;
+
+  TextEditingController messageController = TextEditingController(); 
+  ScrollController scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Hero(
+          tag: 'logo',
+          child: Container(
+            height: 40,
+            child: Image.asset("assets/images/logo.png"),
+          ),
+        ),
+        title: Text("My Chat Room"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: (){
+              _auth.signOut();
+              Navigator.of(context).popUntil((route) => route.isFirst);
+            },
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _firestore.collection("messages").snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return ListView();
+                },
+              ),
+            ),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: messageController,
+                      decoration: InputDecoration(
+                        hintText: "Enter your message ......",
+                      ),
+                    ),
+                  ),
+                  SendButton(
+                    text: 'Send',
+                    callback: () {
+
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class SendButton extends StatelessWidget {
+
+  final String text;
+  final VoidCallback callback;
+
+  const SendButton({ Key key, this.text, this.callback}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return FlatButton(
+      color: Colors.orange,
+      onPressed: callback,
+      child: Text(text),
+    );
   }
 }
